052e48326b5986acc98d199d2310154780019b5a
Hetari <hetari4py@gmail.com>
2025-03-21 02:07:40 +0300

    refactor: formatting

--- resources/js/components/AppShell.vue
+++ resources/js/components/AppShell.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { onMounted, ref } from 'vue';
+  import { SidebarProvider } from '@/components/ui/sidebar';
+  import { onMounted, ref } from 'vue';
 
-interface Props {
+  interface Props {
     variant?: 'header' | 'sidebar';
-}
+  }
 
-defineProps<Props>();
+  defineProps<Props>();
 
-const isOpen = ref(true);
+  const isOpen = ref(true);
 
-onMounted(() => {
+  onMounted(() => {
     isOpen.value = localStorage.getItem('sidebar') !== 'false';
-});
+  });
 
-const handleSidebarChange = (open: boolean) => {
+  const handleSidebarChange = (open: boolean) => {
     isOpen.value = open;
     localStorage.setItem('sidebar', String(open));
-};
+  };
 </script>
 
 <template>
-    <div v-if="variant === 'header'" class="flex min-h-screen w-full flex-col">
-        <slot />
-    </div>
-    <SidebarProvider v-else :default-open="isOpen" :open="isOpen" @update:open="handleSidebarChange">
-        <slot />
-    </SidebarProvider>
+  <div v-if="variant === 'header'" class="flex min-h-screen w-full flex-col">
+    <slot />
+  </div>
+  <SidebarProvider v-else :default-open="isOpen" :open="isOpen" @update:open="handleSidebarChange">
+    <slot />
+  </SidebarProvider>
 </template>
