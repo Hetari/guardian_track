8eced5cd4e598ff72a216a2cd07a396e8a95292c
Hetari <hetari4py@gmail.com>
2025-03-26 00:43:47 +0300

    refactor: simplify class bindings in Reports.vue for better readability

--- resources/js/pages/Reports.vue
+++ resources/js/pages/Reports.vue
@@ -15,9 +15,9 @@
           <img
             class="pointer-events-none z-50 mb-8 inline-block size-20 rounded-full p-3.5"
             :class="{
-              'bg-red-500 brightness-90': circle.accent === 'red',
-              'bg-yellow-500 brightness-90': circle.accent === 'yellow',
-              'bg-green-500 brightness-90': circle.accent === 'green',
+              'bg-red-500': circle.accent === 'red',
+              'bg-yellow-500': circle.accent === 'yellow',
+              'bg-green-500': circle.accent === 'green',
             }"
             :src="circle.svg"
           />
@@ -29,7 +29,7 @@
             {{ circle.body }}
           </p>
           <span
-            class="absolute bottom-8 left-1/2 -z-10 inline-block h-1 w-14 origin-top -translate-x-1/2 rounded transition-all duration-200 ease-in-out group-hover:bottom-0 group-hover:left-0 group-hover:size-full group-hover:-translate-x-0"
+            class="absolute bottom-8 left-1/2 -z-10 inline-block h-1 w-14 !origin-top -translate-x-1/2 rounded transition-all duration-200 ease-in-out group-hover:inset-x-0 group-hover:bottom-0 group-hover:size-full group-hover:-translate-x-0"
             :class="{
               'bg-red-500': circle.accent === 'red',
               'bg-yellow-500': circle.accent === 'yellow',
