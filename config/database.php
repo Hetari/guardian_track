8a492db7f2477f97499c70274d20c6b132ede2ee
Hetari <hetari4py@gmail.com>
2025-03-28 01:48:11 +0300

    fix: set maximum buffer size for PDO MySQL connection and clean up Redis prefix formatting

--- config/database.php
+++ config/database.php
@@ -59,6 +59,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                defined('PDO::MYSQL_ATTR_MAX_BUFFER_SIZE')
+                    ? [PDO::MYSQL_ATTR_MAX_BUFFER_SIZE => 16 * 1024 * 1024]
+                    : [],
             ]) : [],
         ],
 
@@ -147,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
